c95c8f39a1db9c56473c52719820e44501289d2a
countspatula <mcpherson206@gmail.com>
2015-05-18 13:18:28 +0100

    Initial commit including test scene

---
+++ MovieTextureTest/Assets/LookController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider col)
+    {
+
+        Debug.Log("play");
+        col.gameObject.GetComponent<MediaPlayerCtrl>().Play();
+    }
+    void OnTriggerExit(Collider col)
+    {
+
+        Debug.Log("pause");
+        col.gameObject.GetComponent<MediaPlayerCtrl>().Pause();
+    }
+}
